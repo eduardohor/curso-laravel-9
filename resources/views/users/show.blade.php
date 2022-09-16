5b8d26f5e62ee023c60b3adecbf710f1a65db3f3
Eduardo Henrique <eduardo.hor@outlook.com>
2022-09-16 12:24:51 -0300

    add tailwind css

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,17 +4,17 @@
 
 @section('content')
 
-<h1>Listagem do Usuário {{$user->nome}}</h1>
+<h1 class="text-2xl font-semibold leading-tigh py-2">Listagem do Usuário {{$user->nome}}</h1>
 
 <ul>
     <li>Nome: {{$user->name}}</li>
     <li>Email: {{$user->email}}</li>
 </ul>
 
-<form action="{{route('users.delete', $user->id)}}" method="post">
+<form action="{{route('users.delete', $user->id)}}" method="post" class="py-12">
     @csrf
     @method('delete')
-    <button type="submit">Excluir</button>                    
+    <button type="submit" class="rounded-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4">Excluir</button>                    
 </form>
 
 
